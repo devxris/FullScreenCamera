21ccbc00200194d96b958678bb7202187af0fd3c
devxris <chuwei0915@gmail.com>
2017-09-27 12:31:27 +0800

    Simple UI and setup

---
+++ FullScreenCamera/Controller/CameraViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CameraViewController.swift
+//  FullScreenCamera
+//
+//  Created by Chris Huang on 27/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+	@IBOutlet weak var cameraButton: UIButton!
+	
+	@IBAction func capture(_ sender: UIButton) {
+	}
+	
+	@IBAction func unwind(segue: UIStoryboardSegue) {
+		
+	}
+}
---
+++ FullScreenCamera/Controller/PhotoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoViewController.swift
+//  FullScreenCamera
+//
+//  Created by Chris Huang on 27/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+	@IBOutlet weak var imageView: UIImageView!
+	
+	// MARK: Model
+	var image: UIImage? { didSet { imageView.image = image } }
+}
